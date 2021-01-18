19758dbe393fb9f7bbb3e88913471498fcf53194
kwansai724 <kwansai724taka@gmail.com>
2021-01-18 09:23:29 +0900

    5.2終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
-
+  get 'sessions/new'
   root 'static_pages#top'
+  get '/signup', to: 'users#new'
+  resources :users
+
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
