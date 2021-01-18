ddac817757eccf2d8a295651b8dc60371d0677a4
kwansai724 <kwansai724taka@gmail.com>
2021-01-18 20:08:16 +0900

    5.5.3終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,29 @@
 class UsersController < ApplicationController
+before_action :set_user, only: [:show]
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+
+    else
+      render :new
+    end
   end
 
   def show
-    @user = User.find(params[:id])
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
