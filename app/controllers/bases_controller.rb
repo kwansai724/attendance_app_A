1538c197400abac517885936f7fe5336c4dc4589
kwansai724 <kwansai724taka@gmail.com>
2021-01-23 11:41:00 +0900

    拠点一覧モーダル完成

--- app/controllers/bases_controller.rb
+++ app/controllers/bases_controller.rb
@@ -1,5 +1,23 @@
 class BasesController < ApplicationController
+  before_action :set_base, only: [:edit, :destroy]
 
   def index
+    @bases = Base.all
   end
+
+  def edit
+  end
+  
+  def destroy
+    @base.destroy
+    flash[:success] = "#{@base.base_name}を削除しました。"
+    redirect_to bases_url
+  end
+    
+
+  private
+  
+    def set_base
+      @base = Base.find(params[:id])
+    end
 end
