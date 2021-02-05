782a6184500cc304c85de695c807a0137094d872
kwansai724 <kwansai724taka@gmail.com>
2021-02-06 02:32:43 +0900

    attendance.rbのvalidateを無効化

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -5,7 +5,7 @@ class Attendance < ApplicationRecord
   validates :note, length: { maximum: 50 }
 
   validate :finished_at_is_invalid_without_a_started_at
-  validate :change_started_at_than_change_finished_at_fast_is_invalid
+  #validate :change_started_at_than_change_finished_at_fast_is_invalid
 
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
