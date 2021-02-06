e9a273178b804bf6de9ca3ab352fb685e1ed8b93
kwansai724 <kwansai724taka@gmail.com>
2021-02-06 14:35:20 +0900

    changeマイグレーションファイル削除

--- db/migrate/20210130092452_change_data_time_to_attendances.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDataTimeToAttendances < ActiveRecord::Migration[5.1]
-  def change
-    change_column :attendances, :started_at, :time
-    change_column :attendances, :finished_at, :time
-  end
-end
--- db/migrate/20210130093816_change_data_times_to_attendances.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDataTimesToAttendances < ActiveRecord::Migration[5.1]
-  def change
-    change_column :attendances, :started_at, :datetime
-    change_column :attendances, :finished_at, :datetime
-  end
-end
