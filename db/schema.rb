0f4ed715cf148881577ffb97294e14c6519d7dad
kwansai724 <kwansai724taka@gmail.com>
2021-02-09 20:56:41 +0900

    マイグレーションファイル修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210209113130) do
+ActiveRecord::Schema.define(version: 20210209115408) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -62,9 +62,9 @@ ActiveRecord::Schema.define(version: 20210209113130) do
     t.integer "employee_number"
     t.integer "uid"
     t.string "affiliation"
-    t.datetime "designated_work_end_time", default: "2021-02-09 09:00:00"
-    t.datetime "designated_work_start_time", default: "2021-02-09 00:00:00"
     t.datetime "basic_work_time", default: "2021-02-08 23:00:00"
+    t.datetime "designated_work_start_time", default: "2021-02-09 00:00:00"
+    t.datetime "designated_work_end_time", default: "2021-02-09 09:00:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
