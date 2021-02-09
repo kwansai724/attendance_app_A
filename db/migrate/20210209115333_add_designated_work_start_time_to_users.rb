1b200e3f6602c3cd203e4c49f9fc3c3f9c45bc0e
kwansai724 <kwansai724taka@gmail.com>
2021-02-09 21:56:47 +0900

    マイグレーションファイル修正

--- db/migrate/20210209115333_add_designated_work_start_time_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddDesignatedWorkStartTimeToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :designated_work_start_time, :datetime, default: Time.current.change(hour: 9, min: 00, sec: 0)
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210209115408) do
+ActiveRecord::Schema.define(version: 20210209125522) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
